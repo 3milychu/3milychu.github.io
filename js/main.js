274b57464a345e5381285b123cb0bbeb885afe69
3milychu <3milychu@gmail.com>
2017-11-01 17:43:50 -0400

    push

--- js/main.js
+++ js/main.js
@@ -5,13 +5,13 @@ function openNav (){
 
     function closeNav (){
     	//transition the width of our Sidenav div when closed
-    	document.getElementById("mySidenav").style.height="0%";
+    	document.getElementById("mySidenav").style.height="10%";
     }
 
 document.addEventListener('DOMContentLoaded', function(){
 	// alert("Ready");
 
 	var sideNav = document.getElementById("mySidenav");
-	sideNav.innerHTML += "<a href='index.html'>Home</a>";
-	sideNav.innerHTML += "<a href='about.html'>About</a>";
+	// sideNav.innerHTML += "<a href='index.html'>Featured Works</a>";
+	// sideNav.innerHTML += "<a href='about.html'>About</a>";
 });
