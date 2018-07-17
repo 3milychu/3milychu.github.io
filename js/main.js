3db9b433a847697cde98e7cce09fac9a287a1fd9
3milychu <3milychu@gmail.com>
2018-07-17 18:21:37 -0400

    push:

--- js/main.js
+++ js/main.js
@@ -11,25 +11,14 @@ document.addEventListener('DOMContentLoaded', function(){
 
 
     var breadcrumb = document.getElementById("breadcrumb");
-    breadcrumb.innerHTML += "<a style='font-size:1em' href='kmeansfashion.html'>Clustering Images With Kmeans</a>";
-    breadcrumb.innerHTML += "<a style='font-size:1.5em;' href='computationalart.html'>Computational Art</a>";
-    breadcrumb.innerHTML += "<a style='font-size:1.2em;' href='culturaldata.html'>Cultural Data</a>";
-    breadcrumb.innerHTML += "<a style='font-size:1em' href='managerial-landscape.html'>The Managerial Landscape</a>";
-    breadcrumb.innerHTML += "<a style='font-size:1.5em' href='magselector.html'>Interactive Mapping</a>";
-    breadcrumb.innerHTML += "<a style='font-size:1.2em' href='timeplay.html'>Time Play</a>";
-    breadcrumb.innerHTML += "<a style='font-size:1em' href='biometrictracking.html'>Biometric Tracking</a>";
-    breadcrumb.innerHTML += "<a style='font-size:1.5em' href='shortstory.html'>Short Story</a>";
-    breadcrumb.innerHTML += "<a style='font-size:1.2em' href='revive.html'>Revive</a>";
-    breadcrumb.innerHTML += "<a style='font-size:1em' href='informationinmotion.html'>Information in Motion</a>";
-    breadcrumb.innerHTML += "<a style='font-size:1.5em' href='positiveillustration.html'>Positive Illustration</a>";
-    breadcrumb.innerHTML += "<a style='font-size:1.2em' href='ux-health-media.html'>Media for Health App</a>";
-    breadcrumb.innerHTML += "<a style='font-size:1em' href='refreshedresearch.html'>Refreshed Research</a>";
-    breadcrumb.innerHTML += "<a style='font-size:1.5em' href='ai-health-diet.html'>Diet Learning Across the Lifespan</a>";
-    breadcrumb.innerHTML += "<a style='font-size:1.2em' href='roam.html'>Augmenting Intuition &amp; Play</a>";
-    breadcrumb.innerHTML += "<a style='font-size:1em' href='businessreview.html'>Business Review</a>";
-    breadcrumb.innerHTML += "<a style='font-size:1.5em; href='spatialanalysis.html'>Spatial Analysis</a>";
-    breadcrumb.innerHTML += "<a style='font-size:1.2em' href='philosophy.html'>Style</a>";
-    breadcrumb.innerHTML += "<a style='font-size:1em' href='theories.html'>Motivators</a>";
+    breadcrumb.innerHTML += "<a style='font-size:1em' href='kmeansfashion.html'>Forms of Attraction</a>";
+    breadcrumb.innerHTML += "<a style='font-size:1.5em;' href='computationalart.html'>Epoch Runway</a>";
+    breadcrumb.innerHTML += "<a style='font-size:1em;' href='culturaldata.html'>Met.erials</a>";
+    breadcrumb.innerHTML += "<a style='font-size:1.5em' href='csmi.html'>CSMI Insights</a>";
+    breadcrumb.innerHTML += "<a style='font-size:1em' href='motion.html'>Mobile Course Animation</a>";
+    breadcrumb.innerHTML += "<a style='font-size:1.5em' href='ai-health-diet.html'>NuCook</a>";
+    breadcrumb.innerHTML += "<a style='font-size:1em' href='roam.html'>ROAM</a>";
+    breadcrumb.innerHTML += "<a style='font-size:1.5em' href='philosophy.html'>Style</a>";
     breadcrumb.innerHTML += "<a style='font-size:1.5em' href='about.html'>About</a>";
 
 });
