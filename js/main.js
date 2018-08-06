3623c3f225528dbeeb3b8443ea442e6d5c683e8d
3milychu <3milychu@gmail.com>
2018-08-06 11:49:14 -0400

    push

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function(){
     // sideNav.innerHTML += "<a href='https://3milychu.github.io/index.html'>Home</a>";
     // sideNav.innerHTML += "<a href='https://3milychu.github.io/pages/collabs.html'>Collaborations</a>";
     sideNav.innerHTML += "<a href='https://3milychu.github.io/pages/about.html'>About</a>";
-    sideNav.innerHTML += "<a href='https://3milychu.github.io/pages/philosophy.html'>Style</a>";
+    sideNav.innerHTML += "<a href='https://3milychu.github.io/pages/philosophy.html'>Process</a>";
     sideNav.innerHTML += "<a href='https://linkedin.com/in/3milychu' target='_blank'>LinkedIn</a>";
 
 
