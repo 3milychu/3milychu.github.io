42d409296d67f474f94c64c643e574defd6995b2
3milychu <3milychu@gmail.com>
2018-11-15 22:27:53 -0500

    push

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function(){
     // sideNav.innerHTML += "<a href='https://3milychu.github.io/index.html'>Home</a>";
     // sideNav.innerHTML += "<a href='https://3milychu.github.io/pages/collabs.html'>Collaborations</a>";
     sideNav.innerHTML += "<a href='https://3milychu.github.io/pages/about.html'>About</a>";
-    sideNav.innerHTML += "<a href='https://3milychu.github.io/pages/philosophy.html'>Process</a>";
+    // sideNav.innerHTML += "<a href='https://3milychu.github.io/pages/philosophy.html'>Process</a>";
     sideNav.innerHTML += "<a href='https://linkedin.com/in/3milychu' target='_blank'>LinkedIn</a>";
 
 
@@ -14,12 +14,12 @@ document.addEventListener('DOMContentLoaded', function(){
     breadcrumb.innerHTML += "<a style='font-size:1.5em' href='visualvariables.html'>Visualize Uncertainty</a>";
     breadcrumb.innerHTML += "<a style='font-size:1em' href='kmeansfashion.html'>Forms of Attraction</a>";
     breadcrumb.innerHTML += "<a style='font-size:1.5em;' href='computationalart.html'>Epoch Runway</a>";
-    breadcrumb.innerHTML += "<a style='font-size:1em;' href='culturaldata.html'>Met.erials</a>";
-    breadcrumb.innerHTML += "<a style='font-size:1.5em' href='csmi.html'>CSMI Insights</a>";
-    breadcrumb.innerHTML += "<a style='font-size:1em' href='motion.html'>Mobile Course Animation</a>";
-    breadcrumb.innerHTML += "<a style='font-size:1.5em' href='ai-health-diet.html'>NuCook</a>";
-    breadcrumb.innerHTML += "<a style='font-size:1em' href='roam.html'>ROAM</a>";
-    breadcrumb.innerHTML += "<a style='font-size:1.5em' href='philosophy.html'>Style</a>";
+    breadcrumb.innerHTML += "<a style='font-size:1em;' href='culturaldata.html'>Movement for Black Lives</a>";
+    breadcrumb.innerHTML += "<a style='font-size:1.5em;' href='culturaldata.html'>Met.erials</a>";
+    breadcrumb.innerHTML += "<a style='font-size:1em' href='csmi.html'>CSMI Insights</a>";
+    breadcrumb.innerHTML += "<a style='font-size:1.5em' href='motion.html'>Mobile Course Animation</a>";
+    breadcrumb.innerHTML += "<a style='font-size:1em' href='ai-health-diet.html'>NuCook</a>";
+    breadcrumb.innerHTML += "<a style='font-size:1.5em' href='roam.html'>ROAM</a>";
     breadcrumb.innerHTML += "<a style='font-size:1.5em' href='about.html'>About</a>";
 
 });
