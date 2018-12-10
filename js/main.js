8173310ae74b93d8f207bbd003df49a12e68743e
3milychu <3milychu@gmail.com>
2018-12-10 14:55:25 -0500

    message

--- js/main.js
+++ js/main.js
@@ -30,7 +30,36 @@ function openNav (){
     	document.getElementById("mySidenav").style.height ="100%";
     }
 
-    function closeNav (){
-    	//transition the width of our Sidenav div when closed
-    	document.getElementById("mySidenav").style.height="0%";
-    };
+function closeNav (){
+	//transition the width of our Sidenav div when closed
+	document.getElementById("mySidenav").style.height="0%";
+};
+
+function secretMessage() {
+    var secret = document.querySelector('#secret');
+    var secret_form = document.querySelector('input[type="submit"]');
+    var name = document.querySelector('input[name="name"]').value;
+    secret_form.addEventListener("click", function() {
+            name = name.toLowerCase();
+            console.log(name);
+            if(name == "oliver gomes"){
+                secret.innerHTML="";
+                secret.innerHTML="<div class='center_form'><div class='close' id='close_message'>X</div><h2>Hi Oliver,</h2><h3>If you're reading this, I just want to say that I've met with everyone I need to meet with, and Conde is #1. I would be so proud and excited. Please let me know if there's anything I can help with to make things the best they can be for everyone involved. I am open. I have great respect for your vision, enthusiasm and care for the future. You don't see that just anywhere.<br><br>Emily</h3></div>"
+                console.log("secret message sent");
+                closeMessage();
+            } else {
+                secret.innerHTML="";
+                secret.innerHTML="<div class='center_form'><h2>No messages today. Redirecting..</h2>";
+                setTimeout(function(){ 
+                    secret.style.display="none";
+                }, 1000);
+            }
+    });
+};
+
+function closeMessage() {
+    var close_message = document.querySelector('#close_message');
+    close_message.addEventListener("click", function() {
+         secret.style.display="none";
+    })
+};
