3e5a9ebac4ce349979d469e6891e3fe083d0c37a
3milychu@gmail.com <3milychu@gmail.com>
2020-04-08 17:49:24 -0400

    update sidebar scroll

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ contact = document.querySelector('#contact')
 detail = document.querySelector('#detail')
 avatar = document.querySelector('.avatar')
 close = document.querySelector('.fa')
+sidebar = document.querySelector('.sidebar')
+body = document.querySelector('body')
+html = document.documentElement;
 
 
 // scrolling avatar
@@ -30,19 +33,23 @@ function scrollSidebar() {
 
 	} else {
 		window.onscroll=function() {
-			if (praxis.offsetTop-(window.innerHeight*.20) > window.pageYOffset){
-				avatar.style.marginTop="0%"
-				avatar.style.marginLeft="0%"
-				avatar.style.marginRight="0%"
-			} else if (contact.offsetTop-(window.innerHeight*.20) > window.pageYOffset) {
-				avatar.style.marginTop=window.innerHeight/3 + 'px'
-				avatar.style.marginLeft="0%"
-				avatar.style.marginRight="0%"
+			position = window.scrollY
+			height = document.documentElement.scrollHeight - document.documentElement.clientHeight
+			console.log(position, height)
+			percent = position/height
+			rect = sidebar.getBoundingClientRect();
+			sidebar_h = rect.height - avatar.clientHeight
+			border = parseFloat(getComputedStyle(sidebar).borderWidth);
+			console.log(border)
+			console.log(sidebar_h)
+			console.log(percent, d3.format(".0%")(percent))
+			avatar.style.marginLeft="0%"
+			if (percent < .90){
+				avatar.style.marginTop= (percent * sidebar_h)  + "px"
 			} else {
-				avatar.style.marginTop=window.innerHeight/1.38 + 'px'
-				avatar.style.marginLeft="0%"
-				avatar.style.marginRight="0%"
+				avatar.style.marginTop= (percent * sidebar_h) - (border*2)  + "px"
 			}
+			
 		}
 	}
 }
@@ -92,7 +99,7 @@ links[3].onclick=function() {
 	h1.innerHTML='Talks'
 	p = detail.querySelector('p')
 	p.innerHTML="<div class='list'><h3>Upcoming</h3><div class='item'><em>TBD</em><a target ='_blank' href='https://schedule.sxsw.com/2020/events/PP1128191'>Visualizing shared musical experience @ SXSW, Online</a></div></div>"
-	p.innerHTML+="<div class='list'><h3>Past</h3><div class='item'><em>3/23/2020</em><a target ='_blank' href='https://www.crowdcast.io/e/women-in-data-science/register'>Notes on Perspective @ IBM</a></div><div class='item'><em>10/3/2020</em><a target ='_blank' href='https://www.eventbrite.com/e/advance-your-data-career-lightning-talks-and-breakout-sessions-tickets-64644410171#'>Data Analysis in Healthcare @ IBM</a></div><div class='item'><em>6/7/2020</em><a href='https://www.aigany.org/event/fresh-grad-2019-part-2/'>Notes on Perspective @ AIGA NY</a></div></div>"
+	p.innerHTML+="<div class='list'><h3>Past</h3><div class='item'><em>3/23/2020</em><a target ='_blank' href='https://www.crowdcast.io/e/women-in-data-science/register'>Notes on Perspective @ IBM</a></div><div class='item'><em>10/3/2019</em><a target ='_blank' href='https://www.eventbrite.com/e/advance-your-data-career-lightning-talks-and-breakout-sessions-tickets-64644410171#'>Data Analysis in Healthcare @ IBM</a></div><div class='item'><em>6/7/2019</em><a href='https://www.aigany.org/event/fresh-grad-2019-part-2/'>Notes on Perspective @ AIGA NY</a></div></div>"
 	a = detail.querySelector('#link')
 	console.log(a)
 	a.href= ''
